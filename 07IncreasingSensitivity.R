3bf193c3168f73c6cc59aaafa911bceb69c8b9e5
Maxime Keutgen De Greef <mk0964@oceanbio04.princeton.edu>
2024-07-31 16:26:33 -0400

    working on incr sens

--- 07IncreasingSensitivity.R
+++ 07IncreasingSensitivity.R
@@ -1,23 +1,22 @@
-##############################
-### Sensitivity Estimation ###
-##############################
-
-
 library(tidyverse)
 library(robustbase)
 library(gsw)
 library(zoo)
 library(oce)
 library(ggpubr)
-wmolist <- readRDS("~/Documents/ARGO/BGC_Argo_WMO_PSAL_BBP_DOXY_TEMP.rds")
+library(segmented)
+# IDEAS : downscale the residuals, not the original data BUT 
+# BUT filter out profiles that are too monotonic, use segmentate ?
 
+wmolist <- readRDS("~/Documents/ARGO/BGC_Argo_WMO_PSAL_BBP_DOXY_TEMP.rds")
 detected.events.list <- list()
-for (j in c(1:200)) {
+for (j in seq_along(wmolist)) {
   try({
     
     wmo <- wmolist[j]
-
-
+    wmo <- 5904677
+    # cycle_number <- results %>% filter(WMO == wmo) %>% select(CYCLE_NUMBER) %>% unique() %>% as_vector()
+    
     data_df = load_float_data(float_ids = wmo,
                               variables = c("DATA_TYPE", "PLATFORM_NUMBER", "BBP700", "BBP700_dPRES",
                                             "BBP700_ADJUSTED_QC", "LATITUDE", "LONGITUDE", "PROFILE_TEMP_QC",
@@ -66,8 +65,8 @@ for (j in c(1:200)) {
     list_of_tibbles <- data_df %>%
       group_by(CYCLE_NUMBER) %>%
       group_split()
-    list_of_tibbles <- sample(list_of_tibbles, 20)
-    data_df <- list_of_tibbles %>% bind_rows()
+    
+    
     mld.vec <- c()
     for (i in seq_along(list_of_tibbles)) {
       temperature <- list_of_tibbles[[i]]$TEMP_ADJUSTED
@@ -89,13 +88,13 @@ for (j in c(1:200)) {
     
     mld.df <- data.frame(CYCLE_NUMBER = unique(data_df$CYCLE_NUMBER), MLD_DEPTH = mld.vec)
     
-    downscaled_ds_list <- lapply(list_of_tibbles, downscale_data_fun_wo_out)
-    df <- downscaled_ds_list %>% bind_rows()
+    #downscaled_ds_list <- lapply(list_of_tibbles, downscale_data_fun_wo_out)
+    #df <- downscaled_ds_list %>% bind_rows()
     
-    A <- df %>%
+    A <- data_df %>%
       group_by(CYCLE_NUMBER) %>%
       group_modify(~ .x %>%
-                     select(PRES_ADJUSTED, AOU, SPIC, LATITUDE, LONGITUDE, TIME) %>%
+                     dplyr::select(PRES_ADJUSTED, AOU, SPIC, LATITUDE, LONGITUDE, TIME) %>%
                      pivot_longer(!c(PRES_ADJUSTED, LATITUDE, LONGITUDE, TIME), names_to = "VAR", values_to = "VALUE") %>%
                      group_by(VAR) %>%
                      mutate(
@@ -124,6 +123,7 @@ for (j in c(1:200)) {
       ) %>%
       ungroup()
     
+    
     list_of_tibbles <- A %>%
       group_by(CYCLE_NUMBER) %>%
       group_split()
@@ -131,7 +131,7 @@ for (j in c(1:200)) {
     
     downscale_data_fun <- function(df, b = 20, cutoff = 3.3) {
       data <- df %>%
-        select(PRES_ADJUSTED, SCALE.RES.ROB, VAR, CYCLE_NUMBER, LONGITUDE, LATITUDE,TIME) %>%
+        dplyr::select(PRES_ADJUSTED, SCALE.RES.ROB, VAR, CYCLE_NUMBER, LONGITUDE, LATITUDE,TIME) %>%
         pivot_wider(names_from = VAR, values_from = SCALE.RES.ROB)
       
       bin_width <- b
@@ -153,13 +153,15 @@ for (j in c(1:200)) {
       
       return(downscaled_data)
     }
-    
+    # How to remove all those floats where one is clearly monotonic ? 
+
+
     output <- lapply(list_of_tibbles, downscale_data_fun, b = 40)
     output <- output %>% bind_rows()
     
     B <- output %>% bind_rows()
     
-    prop_zero <- B %>% select(AOU, SPIC, CYCLE_NUMBER, PRES_ADJUSTED,LATITUDE,LONGITUDE,TIME) %>%
+    prop_zero <- B %>% dplyr::select(AOU, SPIC, CYCLE_NUMBER, PRES_ADJUSTED,LATITUDE,LONGITUDE,TIME) %>%
       unique() %>%
       pivot_longer(cols = c("AOU", "SPIC"), names_to = "VAR", values_to = "VALUE") %>%
       group_by(CYCLE_NUMBER, VAR) %>% summarize(zero_proportion = mean(VALUE == 0, na.rm = TRUE), .groups = 'drop')
@@ -171,20 +173,38 @@ for (j in c(1:200)) {
       filter(AOU <= 0.5, SPIC <= 0.5) %>%
       pull(CYCLE_NUMBER)
     
-    # filter(OUT.S == 1) %>% is REMOVED so all images are considered 
     
-    carb_eddy.id <- B %>%  select(CYCLE_NUMBER, PRES_ADJUSTED,LATITUDE,LONGITUDE,TIME,OUT.S) %>% unique()
     
-
+    carb_eddy.id <- B %>% filter(OUT.S == 1) %>% dplyr::select(CYCLE_NUMBER, PRES_ADJUSTED,LATITUDE,LONGITUDE,TIME) %>% unique()
+    
+    carb_eddy.id <- carb_eddy.id %>% filter(PRES_ADJUSTED <= 700) %>% filter(PRES_ADJUSTED >= 200)
+    
     
     carb_eddy.id <- carb_eddy.id %>%
       inner_join(mld.df, by = "CYCLE_NUMBER") %>%
       filter(PRES_ADJUSTED > MLD_DEPTH)
-    # Assuming you have the required data and functions loaded
+    
+    const.vec <- c()
+    for (i in 1:nrow(carb_eddy.id)){
+      spic <- data_df %>% filter(CYCLE_NUMBER==carb_eddy.id$CYCLE_NUMBER[i]) %>% select(SPIC,PRES_ADJUSTED) %>% ungroup()
+      
+      # The target pressure level
+      target_pressure <- carb_eddy.id$PRES_ADJUSTED[[i]]
+      
+      # Find the value of spiciness at the pressure level closest to 700
+      closest_spic <- spic %>%
+        slice_min(abs(PRES_ADJUSTED - target_pressure), n = 1) %>%
+        dplyr::select(SPIC) %>% ungroup()
+      
+      # If spic at outlying level is closer to surface value than the mean spic is of the surface value, anomaly is consistent
+      const.vec[i] <- ifelse(abs(closest_spic-spic$SPIC[1]) < abs(mean(spic$SPIC)-spic$SPIC[1]),1,0)
+    }
+    carb_eddy.id$CONSISTENT_ANOM <- const.vec
+    
+    # in this case it's not consistent
     prof.plot <- list()
     res.plot <- list()
     list.plots <- list()
-    
     for (i in seq_along(carb_eddy.id$CYCLE_NUMBER)) {
       current_cycle <- carb_eddy.id$CYCLE_NUMBER[i]
       current_data <- A %>% filter(CYCLE_NUMBER == current_cycle)
@@ -202,16 +222,12 @@ for (j in c(1:200)) {
         theme_bw() +
         labs(x = "Adjusted pressure (dbar)", y = "") +
         theme(legend.position = "bottom") +
-        geom_vline(xintercept = current_eddy$MLD_DEPTH[1], color = "red", alpha = .3, size = 1)
-      
-      if (  sum((current_eddy$OUT.S),na.rm = TRUE )  >= 1 ) {
-        prof.plot[[i]] <- prof.plot[[i]] +
-          geom_vline(xintercept = current_eddy$PRES_ADJUSTED, color = "darkgreen", alpha = .3, size = 1)
-      }
+        geom_vline(xintercept = current_eddy$MLD_DEPTH[1], color = "red", alpha = .3, size = 1) +
+        geom_vline(xintercept = current_eddy$PRES_ADJUSTED, color = "darkgreen", alpha = .3, size = 1)
       
       df <- current_data
       df.ds <- downscale_data_fun(df, b = 40)
-      df.ds <- df.ds %>% ungroup() %>% select(AOU, SPIC, PRES_ADJUSTED) %>%
+      df.ds <- df.ds %>% ungroup() %>% dplyr::select(AOU, SPIC, PRES_ADJUSTED) %>%
         pivot_longer(cols = !PRES_ADJUSTED, names_to = "VAR", values_to = "VALUE")
       
       hline_data <- data.frame(
@@ -231,12 +247,8 @@ for (j in c(1:200)) {
         geom_hline(data = hline_data, aes(yintercept = hline, color = label), inherit.aes = TRUE) +
         scale_color_manual(name = "Threshold", values = c("-3 sigma" = "red", "+3 sigma" = "red", "-2 sigma" = "blue", "+2 sigma" = "blue")) +
         theme(legend.position = "bottom") +
+        geom_vline(xintercept = current_eddy$PRES_ADJUSTED, color = "darkgreen", alpha = .3, size = 1) +
         geom_vline(xintercept = current_eddy$MLD_DEPTH[i], color = "red", alpha = .3, size = 1)
-      
-      if (!is.na(current_eddy$OUT.S[1]) && current_eddy$OUT.S[1] == 1) {
-        res.plot[[i]] <- res.plot[[i]] +
-          geom_vline(xintercept = current_eddy$PRES_ADJUSTED, color = "darkgreen", alpha = .3, size = 1)
-      }
       
       annotation_text <- paste("Cycle Number: ", current_cycle,
                                "\nFloat ID (WMO): ", wmo,
@@ -251,7 +263,7 @@ for (j in c(1:200)) {
     }
     
     if (length(list.plots) > 0) {
-      dir <- paste0("~/Documents/GLOBARGO/figures/SensitivityEstimation/", wmo)
+      dir <- paste0("~/Documents/ARGO/EddySubductionFigures/", wmo)
       if (!dir.exists(dir)) {
         dir.create(dir, recursive = TRUE)
       }
@@ -261,8 +273,8 @@ for (j in c(1:200)) {
         file_name <- paste0(dir, "/", wmo, "_plot_cycle_", cycle_number, ".png")
         ggsave(file_name, list.plots[[k]], width = 10, height = 10)
       }
-    }    
-
+    }
+    
     carb_eddy.id$WMO <- wmo
     detected.events.list[[j]] <- carb_eddy.id
     
@@ -276,13 +288,142 @@ detected.events.list <- lapply(detected.events.list, function(x) {
   return(x)
 })
 
-# Computing Algorithm's Sensitivity :
-data <- read_csv("/data/GLOBARGO/src/data/classification_results_sensitivity(4).csv")
-data
-data %>%
+detected.events.df <- detected.events.list %>% bind_rows()
+
+detected.events.df %>% dplyr::select(CYCLE_NUMBER,WMO) %>% unique()
+
+
+
+write_csv(detected.events.df, "~/Documents/GLOBARGO/data/detected_events.csv")
+
+# Finding out which profiles were already classified
+class.df.part <- read_csv("~/Documents/GLOBARGO/data/classification_results_portion_data.csv")
+class.df.part <- class.df.part %>%  mutate(WMO = str_replace(WMO, "_plot$", ""))
+class.df.part$CYCLE_NUMBER <- class.df.part$Cycle
+
+class.df.part %>% select(CYCLE_NUMBER,WMO)
+detected.events.df %>% select(CYCLE_NUMBER,WMO)
+
+matched_rows <- class.df.part %>%
+  semi_join(detected.events.df, by = c("CYCLE_NUMBER", "WMO"))
+
+write_csv(matched_rows,"~/Documents/GLOBARGO/data/classification_results_portion_data.csv")
+
+# Manually classify data with Python script 05-ClassificationV2.ipynb :
+
+detected.events.df <- read_csv("~/Documents/GLOBARGO/data/detected_events.csv")
+
+manual.class.df <- read_csv("~/Documents/GLOBARGO/data/classification_results_manually_classified.csv")
+manual.class.df <- manual.class.df %>%  mutate(WMO = str_replace(WMO, "_plot$", ""))
+manual.class.df$CYCLE_NUMBER <- manual.class.df$Cycle
+
+# Find if there are outlying profiles 
+# Function to check if a group contains only category 0 or 4
+only_category_0_3_or_4 <- function(categories) {
+  all(categories %in% c(0, 4,3))
+}
+
+# Find WMOs with only category 0 or 4
+wm_only_0_3_or_4 <- manual.class.df %>%
+  group_by(WMO) %>%
+  filter(only_category_0_3_or_4(Category)) %>%
+  summarize(unique_categories = unique(Category))
+
+# Display the result
+print(wm_only_0_or_4)
+
+
+
+
+
+
+
+
+
+# Function to prioritize the category values
+prioritize_category <- function(categories) {
+  if (4 %in% categories) {
+    return(4)
+  } else if (1 %in% categories) {
+    return(1)
+  } else if (2 %in% categories) {
+    return(2)
+  } else if (3 %in% categories) {
+    return(3)
+  } else {
+    return(0)
+  }
+}
+
+# Find WMOs with only category 0 or 4
+anomalous_wmos <- manual.class.df %>%
+  group_by(WMO) %>%
+  filter(only_category_0_3_or_4(Category)) %>%
+  distinct(WMO)
+
+# Remove anomalous floats and apply the function to the data frame
+manual.class.df_filtered <- manual.class.df %>%
+  filter(!WMO %in% anomalous_wmos$WMO) %>%
+  group_by(WMO, Cycle) %>%
+  summarize(Category = prioritize_category(Category), .groups = 'drop') %>%
+  filter(Category != 4)
+
+# Calculate the proportion of each category
+category_proportions <- manual.class.df_filtered %>%
   group_by(Category) %>%
   summarize(Count = n()) %>%
   mutate(Proportion = Count / sum(Count))
-0.0367/0.0167
 
+# Display the result
+print(category_proportions)
+
+# Remove anomalous floats and apply the function to the data frame
+manual.class.df_filtered <- manual.class.df %>%
+  filter(!WMO %in% anomalous_wmos$WMO) %>%
+  group_by(WMO, Cycle) %>%
+  summarize(Category = prioritize_category(Category), .groups = 'drop') %>%
+  filter(Category != 4)
+
+# Calculate the proportion of each category
+category_proportions <- manual.class.df_filtered %>%
+  group_by(Category) %>%
+  summarize(Count = n()) %>%
+  mutate(Proportion = Count / sum(Count))
+
+
+
+# Combine both dataframes
+manual.class.df_filtered <- manual.class.df_filtered %>%
+  rename(CYCLE_NUMBER = Cycle)
+
+# Convert the WMO column in detected.events.df to character type
+detected.events.df <- detected.events.df %>%
+  mutate(WMO = as.character(WMO))
+
+
+# Perform the full join
+combined_df <- full_join(manual.class.df_filtered, detected.events.df, by = c("WMO", "CYCLE_NUMBER"))
+
+# Display the result
+combined_df_filtered <- combined_df %>%
+  filter(!is.na(CONSISTENT_ANOM))
+
+
+write_csv(combined_df_filtered,file = "~/Documents/GLOBARGO/data/classification_results_manually_classified_merged.csv")
+
+# Subduction  events df :
+df <- combined_df_filtered %>% filter(Category %in% c(1,2)) 
+write_csv(df,"~/Documents/GLOBARGO/data/subduction_events.csv")
+combined_df_filtered <- combined_df_filtered %>% filter(Category %in% c(1,2,3,0))
+category_proportions_final <- combined_df_filtered %>%
+  group_by(Category) %>%
+  summarize(Count = n()) %>%
+  mutate(Proportion = Count / sum(Count))
+# A tibble: 4 × 3
+### Category Count Proportion
+###
+### 0   1977    0.497 
+### 1   852     0.214 
+### 2   958     0.241 
+### 3   194     0.0487
 
